3d44f12afad324bf62963b69bd3d1e2e6edff4d6
Fjaramillo1 <fjaramillo1@live.nmhu.edu>
2026-05-18 19:51:44 -0700

    cleaned up file names and requirements.txt

--- Main_test.py
+++
@@ -1,4 +0,0 @@
-import Main
-
-def test_unit_test_test():
-    assert Main.unit_test_test(5) == 25
---
+++ main_test.py
@@ -0,0 +1,4 @@
+import main
+
+def test_unit_test_test():
+    assert main.unit_test_test(5) == 25
